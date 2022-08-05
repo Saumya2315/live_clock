41bb660abbf77b37ececce400e842707c36354f5
Saumya Agarwal <saumya@myillumine.com>
2022-08-05 09:22:13 +0530

    Live clock

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    d = new Date();
+    hourTime = d.getHours();
+    minuteTime = d.getMinutes();
+    secondTime = d.getSeconds();
+    console.log(secondTime)
+    hourRotation = 30*hourTime + minuteTime/2;
+    minuteRotation = 6*minuteTime;
+    secondRotation = 6*secondTime;
+
+    hour.style.transform = `rotate(${hourRotation}deg)`;
+    minute.style.transform = `rotate(${minuteRotation}deg)`;
+    second.style.transform = `rotate(${secondRotation}deg)`;
+}, 1000);
